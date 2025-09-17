933c20dbcdf1fe33c2168f10cf2efae9e99d424d
fahim <fahimahamedweb@gmailo.com|>
2025-09-18 05:08:16 +0600

    added some sections

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\SubcategoryController;
+use App\Http\Controllers\Admin\ChildcategoryController;
+use App\Http\Controllers\Admin\BrandController;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -28,7 +31,39 @@ Route::middleware('admin')->prefix('admin')->group(function() {
         ]
     ]);
  
-
+//subcategory routes
+    Route::resource('subcategories', SubcategoryController::class, [
+        'names' => [
+            'index' => 'admin.subcategories.index',
+            'create' => 'admin.subcategories.create',
+            'store' => 'admin.subcategories.store',
+            'edit' => 'admin.subcategories.edit',
+            'update' => 'admin.subcategories.update',
+            'destroy' => 'admin.subcategories.destroy',
+        ]
+    ]);
+    //childcategory routes
+    Route::resource('childcategories', ChildcategoryController::class, [
+        'names' => [
+            'index' => 'admin.childcategories.index',
+            'create' => 'admin.childcategories.create',
+            'store' => 'admin.childcategories.store',
+            'edit' => 'admin.childcategories.edit',
+            'update' => 'admin.childcategories.update',
+            'destroy' => 'admin.childcategories.destroy',
+        ]
+    ]);
+    //brand routes
+    Route::resource('brands', BrandController::class, [
+        'names' => [
+            'index' => 'admin.brands.index',
+            'create' => 'admin.brands.create',
+            'store' => 'admin.brands.store',
+            'edit' => 'admin.brands.edit',
+            'update' => 'admin.brands.update',
+            'destroy' => 'admin.brands.destroy',
+        ]
+    ]);
 
 
 });
